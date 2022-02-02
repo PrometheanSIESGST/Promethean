60c3c1fa2d9b30c2294acfcb02e1b230f910e1de
Swarup-3112 <swarupaa619yes@gmail.com>
2022-02-02 21:16:20 +0530

    Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import styled from "styled-components";
+import GlobalStyle from "./Globalstyles";
+import { Theme } from "./Theme";
 
-function App() {
+const Header = styled.h1`
+  color:  ${(props) => props.theme.Colors.Header};
+  font-family:  ${(props) => props.theme.Fonts.Arial};
+`
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <Header>All the best #HappyCoding</Header>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/Theme.js
@@ -0,0 +1,29 @@
+export const Theme = {
+  Colors: {
+    PageBg: "#0B1D27",
+    Header: "#FFFFFF",
+    SubHeading: "#CEE002",
+  },
+  Fonts: {
+    Arial: `'Arial', sans-serif`,
+    Poppins: `'Poppins', sans-serif`,
+    Josefin: `'Josefin Sans', sans-serif;`,
+  },
+  MediaQueries: {
+    s: {
+      query: "only screen and (min-width:576px)",
+    },
+    m: {
+      query: "only screen and (min-width:768px)",
+    },
+    l: {
+      query: "only screen and (min-width:992px)",
+    },
+    xl: {
+      query: "only screen and (min-width:1200px)",
+    },
+    xxl: {
+      query: "only screen and (min-width:1400px)",
+    },
+  },
+};
