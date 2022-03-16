13f2373a32317e8ac1c24a5815da5b4cdacaa0d4
Abhishek Tiwari <tiwariabhishek052@gmail.com>
2022-03-16 14:55:52 +0530

    new posters added

--- src/Components/Hero/ImageList.js
+++ src/Components/Hero/ImageList.js
@@ -1,7 +1,7 @@
 const ImageList = [
   {
     id: 1,
-    Image: 'https://unsplash.com/photos/AJaPLsqwCvY/download?ixid=MnwxMjA3fDB8MXxhbGx8MXx8fHx8fDJ8fDE2NDc0MTQyOTU&force=true',
+    Image: "https://unsplash.com/photos/hSwU4sCmbyY/download?ixid=MnwxMjA3fDB8MXxhbGx8MXx8fHx8fDJ8fDE2NDc0MTQ3ODU&force=true"
   },
   {
     id: 2,
@@ -9,8 +9,18 @@ const ImageList = [
     },
   {
     id: 3,
-    Image: "https://unsplash.com/photos/hSwU4sCmbyY/download?ixid=MnwxMjA3fDB8MXxhbGx8MXx8fHx8fDJ8fDE2NDc0MTQ3ODU&force=true"
-    },
+    Image: 'https://unsplash.com/photos/AJaPLsqwCvY/download?ixid=MnwxMjA3fDB8MXxhbGx8MXx8fHx8fDJ8fDE2NDc0MTQyOTU&force=true',
+  },
+
+  {
+    id: 4,
+    Image: 'https://unsplash.com/photos/bXrCL2i5jwc/download?ixid=MnwxMjA3fDB8MXxhbGx8MXx8fHx8fDJ8fDE2NDc0MjEyMTI&force=true',
+  },
+
+  {
+    id: 5,
+    Image: 'https://unsplash.com/photos/JNgDevetoeA/download?ixid=MnwxMjA3fDB8MXxhbGx8Mnx8fHx8fDJ8fDE2NDc0MjEyMTI&force=true',
+  },
 ];
 
 export default ImageList;
