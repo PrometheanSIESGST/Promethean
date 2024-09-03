c55d245df90d50b86400b132ddaa12362f598586
Omkarraut18 <147294172+Omkarraut18@users.noreply.github.com>
2024-09-03 22:26:20 +0530

    Update LandingPage.js

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import styled,{keyframes} from "styled-components";
 import Card from "../Components/Card";
 import ContactUs from "../Components/Contactus";
 import Hero from "../Components/Hero/Hero";
@@ -10,7 +10,9 @@ import HeroImg from "../Images/heroimg.jpg";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Timerworking from "../Components/Timer";
-
+import { Link } from 'react-router-dom';
+import Register from "../Pages/Register";
+import Terratalk from "../Images/terra11.jpg"
 const Main = styled.div`
   max-width: 320px;
   margin: 0 auto;
@@ -49,6 +51,60 @@ const AboutImage = styled.img`
   @media ${(props) => props.theme.MediaQueries.xl.query} {
     margin: ${(props) => (props.right ? "0 120px 0 0" : "0 0 0 80px")};
   }
+  
+`;
+const AboutImage1 = styled.img`
+  margin-top:-50px;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  border-radius: 10px;
+  width: 300px;
+  height: 200px;
+  @media ${(props) => props.theme.MediaQueries.l.query} {
+    margin-right: 40px;
+    width: 450px;
+    height: 350px;
+  }
+  @media ${(props) => props.theme.MediaQueries.xl.query} {
+    margin: ${(props) => (props.right ? "0 120px 0 0" : "0 0 0 80px")};
+  }
+`;
+const Image = styled.img`
+  margin-top:-100px;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  border-radius: 10px;
+  width: 250px;
+  height: 450px;
+  @media ${(props) => props.theme.MediaQueries.m.query} {
+    margin-right: 40px;
+    display:none;
+    width: 350px;
+    height: 350px;
+  }
+`;
+const Hide = styled.img`
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  border-radius: 10px;
+  width: 250px;
+  height: 250px;
+  display: none;
+  @media ${(props) => props.theme.MediaQueries.m.query} {
+    display: block; 
+  }
+  @media ${(props) => props.theme.MediaQueries.l.query} {
+    margin-right: 40px;
+    width: 350px;
+    height: 350px;
+  }
+  @media ${(props) => props.theme.MediaQueries.xl.query} {
+    margin: ${(props) => (props.right ? "0 120px 0 0" : "0 0 0 80px")};
+  }
+  
 `;
 
 const HeroDiv = styled.div`
@@ -94,17 +150,44 @@ const SubHeader = styled.p`
   }
 `;
 
+const rainbowAnimation = keyframes`
+  0% {
+    border-color: red;
+  }
+  16.666% {
+    border-color: orange;
+  }
+  33.333% {
+    border-color: yellow;
+  }
+  50% {
+    border-color: green;
+  }
+  66.666% {
+    border-color: blue;
+  }
+  83.333% {
+    border-color: indigo;
+  }
+  100% {
+    border-color: violet;
+  }
+`;
+
 const RegBtn = styled.span`
-  background-color: ${(props) => props.theme.Colors.SubHeading};
-  border: none;
+  position: relative;
+  display: inline-block;
+  overflow: hidden;
   margin-top: 40px;
-  color: ${(props) => props.theme.Colors.Header};
   padding: 10px 15px;
   text-align: center;
   text-decoration: none;
   font-size: 16px;
   border-radius: 8px;
   cursor: pointer;
+  border: 2px solid transparent;
+  animation: ${rainbowAnimation} 4s linear infinite;
+  background-color:black;
 `;
 
 const BtnLink = styled.a`
@@ -138,14 +221,16 @@ const LandingPage = () => {
         }}
       >
         {/* <Timerworking/> */}
-        <Header>Promethean 2024</Header>
-        <SubHeader>An engineer's solutions to environmental problems</SubHeader>
-        {/* <SubHeader>Register Now !!!</SubHeader>
+        <Header>Promethean 2024- 2025</Header>
+        <SubHeader>An engineer's solutions to environmental problems </SubHeader> <br />
+        <SubHeader>Register For MATRIX 2024 !!!</SubHeader>
         <RegBtn>
-          <BtnLink href="https://forms.office.com/r/4XGMx15TVq" target="_blank">
+          <Link to="/matrix">
+          <BtnLink target="_blank">
             Register Here
           </BtnLink>
-        </RegBtn> */}
+          </Link>
+        </RegBtn>
       </HeroDiv>
       <Main>
         <FlexDiv reverse data-aos="fade-up">
@@ -163,14 +248,14 @@ const LandingPage = () => {
           <Card
             number="02"
             subTitle="Event"
-            title="Promethean"
+            title="Sustain X"
             para="
             Previously called as PROMETHEAN is an annual poster presentation event organized for our new GSTians, our FEs, to convey how they would consider making our earth a better place as an ENGINEER by proposing solutions.
             "
           />
+          <Image src={Terratalk} right/>
         </FlexDiv>
-        <FlexDiv data-aos="fade-up">
-        <AboutImage src={PrometheanImage} />
+        <FlexDiv data-aos="fade-up" >
           <Card
             number="03"
             subTitle="Event"
@@ -179,8 +264,10 @@ const LandingPage = () => {
             We cannot survive without the environment, which is the backbone of our survival. To make people aware of this we invite some committed environmental leaders who are also engineers, who work to ensure that mankind preserves the desirable human-nature interaction.
             "
           />
+          <Hide src={PrometheanImage} />
         </FlexDiv>
         <FlexDiv data-aos="fade-up">
+          <AboutImage1 src={Event2} />
           <Card
             number="04"
             subTitle="Event"
@@ -188,7 +275,6 @@ const LandingPage = () => {
             para="Matrix is annual Quiz organised for our new GSTians, our FEs, where they can use their previous knowledge they have studied so far in a very interesting way.
             "
           />
-        <AboutImage src={Event2} />
         </FlexDiv>      
         <div data-aos="fade-up">{/* <ContactUs /> */}</div>
       </Main>
@@ -197,3 +283,39 @@ const LandingPage = () => {
 };
 
 export default LandingPage;
+
+/* <Main>
+<FlexDiv reverse data-aos="fade-up">
+  <Card
+    number="01"
+    subTitle="About"
+    title="What is Promethean"
+    para="Promethean is inspired by the Greek Titan Prometheus (Ancient Greek Προμηθεύς - Fore thinker ). An innovator, problem solver and a futuristic thinker he was a champion of mankind known for his wily intelligence, who gave fire to mortals and also taught them agriculture so that they could progress and take technology to new levels.
+      It focuses on real world problems happening in and around the world. Engineering is all about making life better, solving problems and allowing humankind to achieve new feats."
+  />
+  <AboutImage src={PrometheanImage} right />
+</FlexDiv>
+<FlexDiv data-aos="fade-up">
+  <AboutImage src={Event1} />
+  <Card
+    number="02"
+    subTitle="Event"
+    title="Promethean"
+    para="
+    PROMETHEAN is an annual poster presentation event organized for our new GSTians, our FEs, to convey how they would consider making our earth a better place as an ENGINEER by proposing solutions.
+    "
+  />
+</FlexDiv>
+<FlexDiv data-aos="fade-up">
+  <Card
+    number="03"
+    subTitle="Event"
+    title="TerraTalk"
+    para="Terra talks stand out from other forms of presentations as an effective medium to convey accurate easy-to-understand information to a target audience. 
+    We cannot survive without the environment, which is the backbone of our survival. To make people aware of this we invite some committed environmental leaders who are also engineers, who work to ensure that mankind preserves the desirable human-nature interaction.
+    "
+  />
+  <AboutImage src={PrometheanImage} right />
+</FlexDiv>
+<div data-aos="fade-up"> <ContactUs /> </div>
+</Main> */
