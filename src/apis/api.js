cbc3f36e5da0c2b772fd3e2db405f7d4aca73eaf
Omkarraut18 <147294172+Omkarraut18@users.noreply.github.com>
2024-09-03 22:17:42 +0530

    Add files via upload

---
+++ src/apis/api.js
@@ -0,0 +1,4 @@
+export const BASE_URL='https://sheetdb.io/api/v1/t9ogsmqu8fy0o';
+// https://sheetdb.io/api/v1/t9ogsmqu8fy0o
+//'https://sheetdb.io/api/v1/e5mvwkq9xzkus'
+//https://django-app-pmh0.onrender.com/api/Team/
